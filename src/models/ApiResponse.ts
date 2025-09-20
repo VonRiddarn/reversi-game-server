2621c60ba8b8c09538221c81854ddac94c4c1097
VonRiddarn <vonriddarn@gmail.com>
2025-09-20 19:54:52 +0200

    feat: implement ApiResponse type and newApiResponse method for normalized responses to the frontend

---
+++ src/models/ApiResponse.ts
@@ -0,0 +1,25 @@
+import { StatusCodes } from "http-status-codes";
+
+export type ApiResponse<T> = {
+	status: StatusCodes;
+	message: string;
+	data: T | null;
+};
+
+export const newApiResponse = <T>(
+	status: StatusCodes,
+	message: string,
+	data: T | null = null
+): ApiResponse<T> => ({
+	status,
+	message,
+	data,
+});
+
+/*
+	"This is not necessary, you can make an inline object and force the type with { ... }: ApiResponse"
+	Yes, that is true, but this way if I ever need to change the signature for any reason I can easily affect all responses.
+	This is a way to normalize the data so that the response parsing is easier for the frontend.
+
+	Eg: If we want to add a timestamp we just do it here instead of trying to fin N places in code.
+*/
