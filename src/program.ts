f5b349715cc82861219d866a856c4a8843cc0363
VonRiddarn <vonriddarn@gmail.com>
2025-09-21 11:38:44 +0200

    refactor: remove Prisma client and schema, update user retrieval to use hardcoded data until we get a new ORM

--- src/program.ts
+++ src/program.ts
@@ -1,12 +1,10 @@
 import express from "express";
-import { PrismaClient } from "@prisma/client";
 import { newApiResponse } from "./models/ApiResponse.js";
 import { StatusCodes } from "http-status-codes";
 import { attachApiResponse } from "./extentions/ResponseExtentions.js";
 import { hashPassword, validatePassword } from "./services/AuthServices.js";
 
 const app = express();
-const prisma = new PrismaClient();
 app.use(attachApiResponse);
 
 // Try some hashing
@@ -16,16 +14,10 @@ console.log(await validatePassword("PweaseProtectMe", hash));
 console.log(await validatePassword("PleaseProtectMe", hash));
 //
 
-app.get("/api/users/:username", async (req, res) => {
+app.get("/api/users/:username", async (_req, res) => {
 	try {
-		const user = await prisma.user.findFirst({
-			where: {
-				username: {
-					equals: req.params.username,
-					mode: "insensitive",
-				},
-			},
-		});
+		// Hard code until we have re-applied a ORM.
+		const user = { id: 1, username: "alice", age: 33 };
 
 		if (!user) return res.apiResponse(newApiResponse(StatusCodes.NOT_FOUND, "User not found."));
 
