d8441d6b37814dd2e2de8bdb0e0d2e8fec21c228
VonRiddarn <vonriddarn@gmail.com>
2025-09-22 21:06:20 +0200

    feat: add entity schema and factory method for creating entities with normalized metadata

---
+++ src/db/schema/_entity.ts
@@ -0,0 +1,18 @@
+import { integer, timestamp } from "drizzle-orm/pg-core";
+
+export const entityColumns = {
+	id: integer().primaryKey().generatedAlwaysAsIdentity(),
+	createdAt: timestamp({ mode: "date" }).defaultNow(),
+	updatedAt: timestamp({ mode: "date" })
+		.defaultNow()
+		.$onUpdate(() => new Date()),
+};
+
+// T extends Record<string, unknown> => I only accept objects with string keys. Unknown doesn't turn off type checking.
+// T & typeof entityColumns - Return type could be infered, but being explicit "feels" right here.
+export const newEntity = <T extends Record<string, unknown>>(table: T): T & typeof entityColumns => {
+	return {
+		...entityColumns,
+		...table,
+	};
+};
