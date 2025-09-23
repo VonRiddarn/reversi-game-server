e182551a967cd98a5a3cf6d2e6879e4618aab1d2
VonRiddarn <vonriddarn@gmail.com>
2025-09-24 01:51:25 +0200

    feat: add database queries to userRepository

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,4 +1,17 @@
-/*
-	I TALK WITH DATABASE!
-	I USE DRIZZLE AND POSTGRE.
-*/
+import { ilike } from "drizzle-orm";
+import { db } from "../db/db.ts";
+import { users } from "../db/schema/users.ts";
+import type { UserSelect } from "../models/entities/User.ts";
+
+export const findAll = async (): Promise<UserSelect[]> => await db.select().from(users);
+
+export const findByUsername = async (username: string): Promise<UserSelect | undefined> =>
+	(await db.select().from(users).where(ilike(users.username, username)).limit(1))[0];
+
+// I can't see in the docs if this is safe: https://orm.drizzle.team/docs/operators#ilike
+// NOTE: Has been manually tested for query injection and is safe. ChatGPT also states that is the case.
+export const findBySearchParam = async (search: string): Promise<UserSelect[]> =>
+	await db
+		.select()
+		.from(users)
+		.where(ilike(users.username, `%${search}%`));
