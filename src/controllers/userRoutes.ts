21e9b81a0bb5aa0a518005459d2b66adc0d1c091
VonRiddarn <vonriddarn@gmail.com>
2025-09-23 19:00:22 +0200

    feat: add user routes, repository, and services files with initial comments

---
+++ src/controllers/userRoutes.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+export const ROOT = "/api/users";
+
+const router = express.Router();
+
+/*
+	I AM MIDDLEWARE. I CONNECT GET AND POST REQUESTS.
+*/
+
+export default router;
