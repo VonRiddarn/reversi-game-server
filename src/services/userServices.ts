a27220cb99cc29c1e062949173b94929c2f8607f
VonRiddarn <vonriddarn@gmail.com>
2025-09-24 01:52:12 +0200

    feat: add an abstraction service layer to the userRepository through userService

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,5 +1,24 @@
-/* 
-	I HANDLE BUSINESS LOGIC.
-	I MAKE CALLS TO REPOSITORY, BUT NEVER TO THE DATABASE!
-	THAT WOULD BREAK THE EXTRACTION LAYER.
-*/
+import { toUserPublicOut, toUserPublicOutArray, type UserPublicOut } from "../models/dtos/UserPublicOut.ts";
+import type { UserSelect } from "../models/entities/User.ts";
+import * as userRepository from "../repositories/userRepository.ts";
+
+export const getUsersAllInternal = async (): Promise<UserSelect[]> => await userRepository.findAll();
+
+export const getUsersAll = async (): Promise<UserPublicOut[]> =>
+	toUserPublicOutArray(await getUsersAllInternal());
+
+export const getUsersByUsernameInternal = async (username: string): Promise<UserSelect | undefined> =>
+	await userRepository.findByUsername(username);
+
+export const getUserByUsername = async (username: string): Promise<UserPublicOut | undefined> => {
+	const user = await getUsersByUsernameInternal(username);
+	if (!user) return undefined;
+
+	return toUserPublicOut(user);
+};
+
+export const getUsersBySearchParamInternal = async (search: string): Promise<UserSelect[]> =>
+	await userRepository.findBySearchParam(search);
+
+export const getUsersBySearchParam = async (search: string): Promise<UserPublicOut[]> =>
+	toUserPublicOutArray(await getUsersBySearchParamInternal(search));
