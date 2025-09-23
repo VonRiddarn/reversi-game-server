07ab71d65a4ed6fba8501628c70e010565eac087
VonRiddarn <vonriddarn@gmail.com>
2025-09-24 01:56:08 +0200

    refactor: correct userService import references in userEndpoints

--- src/controllers/userEndpoints.ts
+++ src/controllers/userEndpoints.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import * as userServices from "../services/userService.ts";
+import * as userService from "../services/userService.ts";
 import type { UserPublicOut } from "../models/dtos/UserPublicOut.ts";
 import { newApiResponse } from "../models/ApiResponse.ts";
 import { StatusCodes } from "http-status-codes";
@@ -12,8 +12,8 @@ userRouter.get("/", async (req, res) => {
 	const searchParam = req.query.search as string | undefined;
 	try {
 		const userDto: UserPublicOut[] = searchParam
-			? await userServices.getUsersBySearchParam(searchParam)
-			: await userServices.getUsersAll();
+			? await userService.getUsersBySearchParam(searchParam)
+			: await userService.getUsersAll();
 		if (!userDto || userDto.length <= 0)
 			return res.apiResponse(newApiResponse(StatusCodes.NOT_FOUND, "No users found."));
 
@@ -33,7 +33,7 @@ userRouter.get("/:username", async (req, res) => {
 	const { username } = req.params;
 
 	try {
-		const userDto = userServices.getUserByUsername(username);
+		const userDto = userService.getUserByUsername(username);
 
 		if (!userDto) return res.apiResponse(newApiResponse(StatusCodes.NOT_FOUND, "User not found."));
 
