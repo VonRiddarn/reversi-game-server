cce15070a3b6fd9885b3e857a256cabcbfcd5e1b
VonRiddarn <vonriddarn@gmail.com>
2025-09-24 16:59:50 +0200

    feat: implement invite repository with methods for finding invites

---
+++ src/repositories/inviteRepository.ts
@@ -0,0 +1,20 @@
+import { ilike, eq } from "drizzle-orm";
+import { db } from "../db/db.ts";
+import { users } from "../db/schema/users.ts";
+import type { InviteSelect } from "../models/entities/Invite.ts";
+import { invites } from "../db/schema/invites.ts";
+
+export const findAll = async (): Promise<InviteSelect[]> => await db.select().from(invites);
+
+export const findByRedeemCode = async (redeemCode: string): Promise<InviteSelect | undefined> =>
+	(await db.select().from(invites).where(eq(invites.redeemCode, redeemCode)).limit(1))[0];
+
+export const findByCreatorUsername = async (username: string): Promise<InviteSelect[]> => {
+	const rows = await db
+		.select()
+		.from(invites)
+		.innerJoin(users, eq(invites.creatorId, users.id))
+		.where(ilike(users.username, username));
+
+	return rows.map((r) => r.invites);
+};
