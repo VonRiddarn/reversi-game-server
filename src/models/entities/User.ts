b4ad13beef4ffcc31d8b2b8b7433e256214758b1
VonRiddarn <vonriddarn@gmail.com>
2025-09-25 01:10:08 +0200

    feat: add constants for username and password validation in User model

--- src/models/entities/User.ts
+++ src/models/entities/User.ts
@@ -4,3 +4,13 @@ import type { users } from "../../db/schema/users.ts";
 export type UserInsert = typeof users.$inferInsert;
 /** Type for database selection. */
 export type UserSelect = typeof users.$inferSelect;
+
+export const FORBIDDEN_NAMES = ["admin", "support", "root", "moderator"] as const;
+
+export const USERNAME_LENGTH_MIN = 6 as const;
+export const USERNAME_LENGTH_MAX = 28 as const;
+
+// https://pages.nist.gov/800-63-4/sp800-63b/passwords/
+// https://cheatsheetseries.owasp.org/cheatsheets/Authentication_Cheat_Sheet.html
+export const PASSWORD_LENGTH_MIN = 8;
+export const PASSWORD_LENGTH_MAX = 64;
