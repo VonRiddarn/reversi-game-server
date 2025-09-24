7816183923fa759b6b25928423e4dc0566742029
VonRiddarn <vonriddarn@gmail.com>
2025-09-25 01:14:24 +0200

    feat: add InviteUpdate type for database update using OmitEntity

--- src/models/entities/Invite.ts
+++ src/models/entities/Invite.ts
@@ -1,6 +1,9 @@
 import type { invites } from "../../db/schema/invites.ts";
+import type { OmitEntity } from "../types/OmitEntity.ts";
 
 /** Type for database insertion. */
 export type InviteInsert = typeof invites.$inferInsert;
 /** Type for database selection. */
 export type InviteSelect = typeof invites.$inferSelect;
+/** Type for database update. */
+export type InviteUpdate = OmitEntity<InviteSelect>;
