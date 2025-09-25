091ec15626dd65f16843dba932fb5469af6966ca
VonRiddarn <vonriddarn@gmail.com>
2025-09-25 02:24:24 +0200

    fix: commit authtoken constant used in entity creation

--- src/models/entities/AuthToken.ts
+++ src/models/entities/AuthToken.ts
@@ -4,3 +4,5 @@ import type { authTokens } from "../../db/schema/authTokens.ts";
 export type AuthTokenInsert = typeof authTokens.$inferInsert;
 /** Type for database selection. */
 export type AuthTokenSelect = typeof authTokens.$inferSelect;
+
+export const AUTH_TOKEN_VALID_DAYS: number = 10 as const;
